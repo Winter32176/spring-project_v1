fc11fe4a477e33d64fd719f1ab6ac032684566ce
Robert <robert32176@outlook.com>
2026-02-10 09:47:08 +0200

    init

---
+++ src/main/java/com/spring/spring_project/Security/SecurityConfig.java
@@ -0,0 +1,23 @@
+package com.spring.spring_project.Security;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+public class SecurityConfig {
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        http
+                .authorizeHttpRequests(auth -> auth
+                        .requestMatchers("/", "/css/**", "/js/**", "/images/**", "/style.css").permitAll()
+                        .anyRequest().authenticated()
+                )
+                .formLogin(Customizer.withDefaults());
+
+        return http.build();
+    }
+}
